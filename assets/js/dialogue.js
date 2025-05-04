2861ab3ed0482873eac61fee967e626f1db6ac71
cherr-pi <co.sld@proton.me>
2025-05-04 03:50:45 +0000

    feat: add

--- assets/js/dialogue.js
+++ assets/js/dialogue.js
@@ -6,23 +6,26 @@ document.addEventListener('DOMContentLoaded', function () {
   paragraphs.forEach((p) => {
     let html = p.innerHTML;
 
-    // double-quoted dialogue: "text" and “text”
+    // match “double quotes” (or regular ")
     html = html.replace(
-      /(?:["“”])([^"“”]+)(?:["“”])/g,
-      '<span class="double-quoted">“$1”</span>'
+      /(["“”])([^"“”]+?)\1/g,
+      '<span class="double-quoted">“$2”</span>'
     );
 
-    // single-quoted dialogue: 'text' and ‘text’
+    // match ‘single quotes’ (or regular ')
     html = html.replace(
-      /(?:['‘’])([^'‘’]+)(?:['‘’])/g,
-      "<span class='single-quoted'>‘$1’</span>"
+      /(['‘’])([^'‘’]+?)\1/g,
+      "<span class='single-quoted'>‘$2’</span>"
     );
 
     // bold text: **text**
-    html = html.replace(/\*\*([^*]+)\*\*/g, "<span class='bold'>$1</span>");
+    html = html.replace(/\*\*([^*]+?)\*\*/g, "<span class='bold'>$1</span>");
 
     // italic text: *text*
-    html = html.replace(/\*([^*]+)\*/g, "<span class='italic'>$1</span>");
+    html = html.replace(
+      /(^|[^*])\*([^*]+?)\*(?!\*)/g,
+      "$1<span class='italic'>$2</span>"
+    );
 
     p.innerHTML = html;
   });
